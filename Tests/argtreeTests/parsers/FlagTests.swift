c3799fa9101652296b4112124fb66f52a1486943
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2018-05-01 17:41:13 +0200

    fixed FlagTests.testAccessingFlagLater.

--- Tests/argtreeTests/parsers/FlagTests.swift
+++ Tests/argtreeTests/parsers/FlagTests.swift
@@ -67,10 +67,12 @@ final class FlagTests: XCTestCase {
         let verboseFlag = Flag(longName: "verbose", shortName: "v")
         let argTree: ArgTree = ArgTree(parsers: [verboseFlag])
 
+        verboseFlag.values.removeAll()
         try! argTree.parse(arguments: ["foo", "-v"])
         var verbose = verboseFlag.values.first != nil
         XCTAssert(verbose)
 
+        verboseFlag.values.removeAll()
         try! argTree.parse(arguments: ["foo"])
         verbose = verboseFlag.values.first != nil
         XCTAssert(!verbose)

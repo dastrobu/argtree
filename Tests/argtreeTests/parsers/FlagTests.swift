2e37495d5299bde4b41ec4df1658b06dd7a9f906
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2018-05-01 17:55:23 +0200

    removed bad test definition on linux.

--- Tests/argtreeTests/parsers/FlagTests.swift
+++ Tests/argtreeTests/parsers/FlagTests.swift
@@ -81,7 +81,6 @@ final class FlagTests: XCTestCase {
 #if !os(macOS)
     static var allTests = [
         ("testAccessingFlagLater", testAccessingFlagLater),
-        ("testCommandParsing", testCommandParsing),
         ("testCustomPrefix", testCustomPrefix),
         ("testFlagNotParsableTwice", testFlagNotParsableTwice),
         ("testFlagParsing", testFlagParsing),

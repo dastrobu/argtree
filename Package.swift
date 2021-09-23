b041b4593149b699d21b036f48f2ecedea9c8704
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2021-09-23 19:11:43 +0200

    bump swift-log to 1.4.2
    Signed-off-by: Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             ]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
     ],
     targets: [
         .target(
